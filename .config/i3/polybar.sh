4568d0bde71b7b0585f2d74e692b23145050fa07
Chase Weaver <chase.weaver34@gmail.com>
2020-02-03 15:39:12 -0500

    Update polybar.sh

--- .config/i3/polybar.sh
+++ .config/i3/polybar.sh
@@ -7,4 +7,12 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-polybar top &
+# polybar top &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
